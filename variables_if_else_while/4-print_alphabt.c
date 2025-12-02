80fdd10f41506112d46d9c0fcde0471df4205f90
root <jhashimov17871@ada.edu.az>
2025-12-02 10:04:04 +0000

    Done

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -10,13 +10,14 @@ int main(void)
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-    {
-        if (c == 'q' || c == 'e')
-            continue;
-        
-        putchar(c);
-    }
+	{
+		if (c == 'q' || c == 'e')
+			continue;
 
-    putchar('\n');
+		putchar(c);
+	}
+
+	putchar('\n');
+
 	return (0);
 }
