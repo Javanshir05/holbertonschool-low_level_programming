61d00815a9011c6b28ba1d1168dadf1f341cf2f3
root <jhashimov17871@ada.edu.az>
2025-12-02 10:07:08 +0000

    Done

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers from 0
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+
+    for(a = 0; a < 10; a++)
+        printf(a);
+
+    printf("\n");
+
+	return (0);
+}
