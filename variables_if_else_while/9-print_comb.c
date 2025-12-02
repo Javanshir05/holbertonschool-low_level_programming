cc31b7827d39927aa32c85819a3798f29992ce76
root <jhashimov17871@ada.edu.az>
2025-12-02 11:09:25 +0000

    Done

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -7,22 +7,17 @@
  */
 int main(void)
 {
-	int i, j;
+	int a;
 
-	for (i = 0; i <= 8; i++)
+	for (a = 0; a < 10; a++)
 	{
-		for (j = i + 1; j <= 9; j++)
+		putchar(a + '0');
+		if (a != 9)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-
-			if (!(i == 8 && j == 9))
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
-
+	putchar('\n');
 	return (0);
 }
