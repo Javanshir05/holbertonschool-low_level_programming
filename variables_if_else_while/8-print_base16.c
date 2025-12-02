546b50412d7d03b5b826ab8814174295de25698e
root <jhashimov17871@ada.edu.az>
2025-12-02 10:29:38 +0000

    Done

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 /**
- * main - prints the lowercase alphabet in reverse
+ * main - prints all the numbers of base 16 in lowercase
  *
  * Return: Always 0.
  */
 int main(void)
 {
 	int a;
-    char c;
+	char c;
 
-    for (a = 0; a < 10; a++)
-        putchar(a + '0');
+	for (a = 0; a < 10; a++)
+		putchar(a + '0');
 
 	for (c = 'a'; c <= 'f'; c++)
 		putchar(c);
