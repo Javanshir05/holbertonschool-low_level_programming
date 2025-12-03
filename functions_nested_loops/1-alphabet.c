271be5ba0cb76bfb6611522e2df43cb2c5661e68
root <jhashimov17871@ada.edu.az>
2025-12-03 06:40:28 +0000

    Done

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -10,13 +10,15 @@ void print_alphabet(void)
     char c;
 
     for (c = 'a'; c <= 'z'; c++)
+    {
         putchar(c);
+    }
 
     putchar('\n');
 }
 
 int main(void)
 {
-    printf(print_alphabet);
+    print_alphabet();
     return 0;
 }
