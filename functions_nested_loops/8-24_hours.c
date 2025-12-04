987a3f6b655a9bf3767e5dd53d7340ac4103560a
root <jhashimov17871@ada.edu.az>
2025-12-04 09:21:24 +0000

    Done

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			printf("%02d:%02d\n", h, m);
+		}
+	}
+}
