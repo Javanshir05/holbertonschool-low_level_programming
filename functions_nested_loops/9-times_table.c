2c3982c224f02f4519e400077e1f88ed066c44b3
root <jhashimov17871@ada.edu.az>
2025-12-04 09:26:21 +0000

    Done

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			_putchar(j + '0');
+		}
+	}
+}
