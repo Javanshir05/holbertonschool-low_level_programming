2419a01c04c28f967ac3d21330af76a48f412933
root <jhashimov17871@ada.edu.az>
2025-12-04 09:31:53 +0000

    Done

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: integer to compute absolute value for
+ *
+ * Return: absolute value of n
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
