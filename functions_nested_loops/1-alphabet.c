e824448045359e551e4a47c8a081575e4bc0f975
root <jhashimov17871@ada.edu.az>
2025-12-04 08:31:10 +0000

    Done

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints the alphabet in lowercase followed by a new line
+ * main - prints the alphabet in lowercase followed by a new line
  *
  * Return: void
  */
@@ -16,4 +16,3 @@ void print_alphabet(void)
 
 	_putchar('\n');
 }
-
---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints 10 times the alphabet followed by a new line
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c;
+    int i;
+    for (i = 0; i <+ 10; i++)
+    {
+	    for (c = 'a'; c <= 'z'; c++)
+	    {
+		    _putchar(c);
+	    }
+    }
+
+	_putchar('\n');
+}
