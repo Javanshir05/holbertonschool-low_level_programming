497591a2604cde3b9e4bd09f0606fab4d8ae8e83
root <jhashimov17871@ada.edu.az>
2025-12-04 09:15:07 +0000

    Done

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,12 @@
  */
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+	if (last_digit < 0)
+    {
+        last_digit = - last_digit
+    }
+    int last_digit = n % 10;
+    
     printf("%d", last_digit);
 
 	return (last_digit);
