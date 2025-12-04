498a65f7061fb6282cd46fe704804b8ba7bb9038
root <jhashimov17871@ada.edu.az>
2025-12-04 09:12:58 +0000

    Done

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: integer to compute absolute value for
+ *
+ * Return: absolute value of n
+ */
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+    printf("%d", last_digit);
+
+	return (0);
+}
