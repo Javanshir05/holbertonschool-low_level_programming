54bf1f691dda9e142c08b74a2233a03852d2aecf
root <jhashimov17871@ada.edu.az>
2025-12-04 08:27:09 +0000

    Done

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,19 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the alphabet in lowercase
+ * print_alphabet - Prints the alphabet in lowercase followed by a new line
+ *
+ * Return: void
  */
 void print_alphabet(void)
 {
-    char c;
+	char c;
 
-    for (c = 'a'; c <= 'z'; c++)
-        _putchar(c);  
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
 
-    _putchar('\n');    
+	_putchar('\n');
 }
+
--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,10 +1,10 @@
-#include "main.h"
+// #include "main.h"
 
-/**
-* Return: Always 0
-*/
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
+// /**
+// * Return: Always 0
+// */
+// int main(void)
+// {
+//     print_alphabet();
+//     return (0);
+// }
