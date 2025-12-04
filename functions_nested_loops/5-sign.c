ef2a17b3ff12041a63226443b5b0944494f9b2cf
root <jhashimov17871@ada.edu.az>
2025-12-04 08:51:58 +0000

    Done

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if a character is a letter
+ * @c: the character to check
+ *
+ * Return: 1 if c is a letter (lowercase or uppercase), 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (n > 0)
+	{
+        printf("+");
+		return (1);
+	}
+    else if (n == 0)
+    {
+        printf("0");
+        return (0)
+    }
+    else 
+    {
+        printf("-");
+        return (-1);
+    }
+
+	return (0);
+}
