057de22ae4f9b0db6bb3e52484054f387aae7648
root <jhashimov17871@ada.edu.az>
2025-12-05 09:45:03 +0000

    Done

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: character to check
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int mul(int a, int b)
+{
+    int result = a * b;
+
+    printf("%d", result);
+
+	return 0;
+}
