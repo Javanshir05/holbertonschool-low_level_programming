6eac577689f24c49aadf2b1a377fecaacba45cf9
root <jhashimov17871@ada.edu.az>
2025-12-05 09:49:19 +0000

    Done

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+/* 2-mul.c */
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the product of a and b
+ */
+void print_numbers(void)
+{
+    for (int i = 0; i <= 9; i++)
+        putchar(i + '0')
+
+    putchar('\n')
+	return (0);
+}
