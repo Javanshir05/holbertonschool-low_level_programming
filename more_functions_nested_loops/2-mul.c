6cb24830d42ea6476af395fa2bb6bc90d6d80ef1
root <jhashimov17871@ada.edu.az>
2025-12-05 09:47:31 +0000

    Done

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,12 +1,14 @@
+/* 2-mul.c */
 #include "main.h"
 
 /**
- * _isdigit - checks for a digit (0 through 9)
- * @c: character to check
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
  *
- * Return: 1 if c is a digit, 0 otherwise
+ * Return: the product of a and b
  */
 int mul(int a, int b)
 {
-	return (a*b);
+	return (a * b);
 }
