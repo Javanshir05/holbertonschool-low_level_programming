4e5f4b752171cd9609639326fd6d1fc777fdfc2f
root <jhashimov17871@ada.edu.az>
2025-12-05 10:19:49 +0000

    Done

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
 
+/**
+ * main - prints numbers from 1 to 100 with FizzBuzz rules
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 1; i <= 100; i++)
-    {
-        if (i % 3 == 0 && i % 5 == 0)
-        {
-            printf("FizzBuzz");
-        }
-        else if (i % 3 == 0)
-        {
-            printf("Fizz");
-        }
-        else if (i % 5 == 0)
-        {
-            printf("Buzz");
-        }
-        else
-        {
-            printf("%d", i);
-        }
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
 
-        if (i != 100)
-            printf(" ");
-    }
+		if (i != 100)
+			printf(" ");
+	}
 
-    printf("\n");
-    return (0);
+	printf("\n");
+
+	return (0);
 }
