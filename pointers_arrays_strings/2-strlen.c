d7b63a5077a582bbe2e237f3cada3fcbe4036c26
root <jhashimov17871@ada.edu.az>
2025-12-10 13:21:06 +0000

    Done

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,13 @@
 #include "main.h"
-#include <stdio.h>
+#include <string.h>
 
+/**
+ * _strlen - returns the length of a string
+ * @s: string to measure
+ *
+ * Return: length of the string
+ */
 int _strlen(char *s)
 {
-    return strlen(s);
+	return (strlen(s));
 }
