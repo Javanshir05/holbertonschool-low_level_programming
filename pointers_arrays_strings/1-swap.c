8af59c9ca6e7099bfd33ac9be2a3b516d9287083
root <jhashimov17871@ada.edu.az>
2025-12-10 13:14:41 +0000

    Done

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,10 +1,15 @@
 #include "main.h"
 
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer to first int
+ * @b: pointer to second int
+ */
 void swap_int(int *a, int *b)
 {
-    int c;
+	int tmp;
 
-    c = *a;
-    *a = *b;
-    *b = c;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,15 +1,7 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * swap_int - swaps the values of two integers
- * @a: pointer to first int
- * @b: pointer to second int
- */
-void swap_int(int *a, int *b)
-{
-	int tmp;
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
+#endif 
