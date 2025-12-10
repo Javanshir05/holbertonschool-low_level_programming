2162f0c428b8012b72f57a6886af53942e406243
root <jhashimov17871@ada.edu.az>
2025-12-10 13:13:29 +0000

    Done

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer to first int
+ * @b: pointer to second int
+ */
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 
