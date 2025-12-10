9bbb4cfe2af70deb90c2605c28789e49105efe7a
root <jhashimov17871@ada.edu.az>
2025-12-10 13:36:01 +0000

    Done

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,6 +1,17 @@
 #include "main.h"
 
+/**
+ * _puts - prints a string followed by a new line to stdout
+ * @str: string to print
+ */
 void _puts(char *str)
 {
-    puts("%s\n", str);
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
