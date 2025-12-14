27decbd359235a8fc090e71bad73f2134c964fe3
root <jhashimov17871@ada.edu.az>
2025-12-14 14:33:00 +0000

    Done

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -3,17 +3,20 @@
 /**
  * print_chessboard - prints an 8x8 chessboard
  * @a: 2D array representing the chessboard
+ *
+ * Return: void
  */
 void print_chessboard(char (*a)[8])
 {
-    int i, j;
+	int i;
+	int j;
 
-    for (i = 0; i < 8; i++)
-    {
-        for (j = 0; j < 8; j++)
-        {
-            _putchar(a[i][j]);
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
 }
