86adb3510ae781efcc700f62cb85a9e7bb149719
root <jhashimov17871@ada.edu.az>
2025-12-14 13:51:06 +0000

    Done

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -3,26 +3,21 @@
 /**
  * print_rev - Prints a string in reverse, followed by a new line.
  * @s: The string to be printed in reverse.
- *
- * This function calculates the length of the string by iterating
- * until the null terminator ('\0') is found. Then, it iterates
- * backward from the last character (length - 1) down to index 0,
- * printing each character. Finally, it prints a newline.
  */
 void print_rev(char *s)
 {
-    int length = 0;
-    int i;
+	int length = 0;
+	int i;
 
-    while (s[length] != '\0')
-    {
-        length++;
-    }
+	while (s[length] != '\0')
+	{
+		length++;
+	}
 
-    for (i = length - 1; i >= 0; i--)
-    {
-        _putchar(s[i]);
-    }
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
