fcf258782ffd5b9565627f60a9c7d0e8462d7483
root <jhashimov17871@ada.edu.az>
2025-12-14 13:53:35 +0000

    Done

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,15 +9,18 @@ void print_rev(char *s)
 	int length = 0;
 	int i;
 
+	/* Calculate the length of the string */
 	while (s[length] != '\0')
 	{
 		length++;
 	}
 
+	/* Print the string in reverse order */
 	for (i = length - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
 
+	/* Print a new line */
 	_putchar('\n');
 }
