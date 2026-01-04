6ac9e292ce1618b711152cd28f25203c06899afd
root <jhashimov17871@ada.edu.az>
2026-01-04 18:42:19 +0000

    Done

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -18,4 +18,14 @@ void print_name(char *name, void (*f)(char *));
  */
 void array_iterator(int *array, size_t size, void (*action)(int));
 
+/**
+ * int_index - searches for an integer
+ * @array: array of integers
+ * @size: number of elements in the array
+ * @cmp: pointer to function to compare values
+ *
+ * Return: index of first matching element or -1
+ */
+int int_index(int *array, int size, int (*cmp)(int));
+
 #endif /* FUNCTION_POINTERS_H */
