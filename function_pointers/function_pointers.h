15d41ea2db8642f87ae83cfebb23c0262c8645a5
root <jhashimov17871@ada.edu.az>
2026-01-04 18:40:47 +0000

    Done

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,11 +1,21 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#include <stddef.h>
+
 /**
  * print_name - prints a name using a function pointer
  * @name: name of the person
  * @f: pointer to function that prints the name
  */
 void print_name(char *name, void (*f)(char *));
+
+/**
+ * array_iterator - executes a function on each element of an array
+ * @array: array of integers
+ * @size: size of the array
+ * @action: pointer to function to execute
+ */
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* FUNCTION_POINTERS_H */
