5e2ab58a01f4f0e1891143ed1df2d2ba3532361d
root <jhashimov17871@ada.edu.az>
2026-01-04 18:37:50 +0000

    Done

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,11 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+/**
+ * print_name - prints a name using a function pointer
+ * @name: name of the person
+ * @f: pointer to function that prints the name
+ */
 void print_name(char *name, void (*f)(char *));
 
 #endif 
