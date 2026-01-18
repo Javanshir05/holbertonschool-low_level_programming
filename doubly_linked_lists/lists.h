655825e4d7e3c43f2f8ec3c9fa938bf0e70d842c
root <jhashimov17871@ada.edu.az>
2026-01-18 15:32:26 +0000

    Done

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -12,7 +12,6 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * for Holberton project
  */
 typedef struct dlistint_s
 {
@@ -21,10 +20,10 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
-/* Task 0 */
 size_t print_dlistint(const dlistint_t *h);
-
-/* Task 1 */
 size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
 
 #endif /* LISTS_H */
